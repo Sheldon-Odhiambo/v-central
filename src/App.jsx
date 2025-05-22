7a7066ce2e23b85fd61cdbb3de484e4fd876a3c2
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2025-05-22 21:17:22 +0300

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import VolunteerSignup from "./components/VolunteerSignup";
 import OrganizationSignup from "./components/OrganizationSignup";
+import AttacheeSignup from "./components/ AttacheeSignup";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import Foooter from "./components/Foooter";
@@ -26,6 +27,7 @@ function App() {
       <Route path="navbar" element={<Navbar/>}/>
       <Route path="/signup/volunteer" element={<VolunteerSignup />} />
       <Route path="/signup/organization" element={<OrganizationSignup/>}/>
+      <Route path="/signup/attachee" element={<AttacheeSignup/>}/>
       <Route path="/signup" element={<SignUp/>}/>
       <Route path="/signin" element={<SignIn/>}/>
       <Route path="/foooter" element={<Foooter/>}/>
@@ -33,6 +35,7 @@ function App() {
       <Route path="/about" element={<About/>}/>
       <Route path="/volunteerdashboard" element={<VolunteerDashboard/>}/>
       <Route path="/organizationdashboard" element={<OrganizationDashboard/>}/>
+      
 
 
 
