85df09598858e4fa08f7fa01876dd073f7034e55
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2025-05-23 23:35:51 +0300

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import SignIn from "./components/SignIn";
 import Foooter from "./components/Foooter";
 import Footer from "./components/Footer";
 import About from "./components/About";
-import VolunteerDashboard from "./components/VolunteerDashboard";
-import OrganizationDashboard from "./components/OrganizationDashboard";
+import ApprenticeSignup from "./components/ApprenticeSignup";
+import InternSignup from "./components/InternSignup";
 
 
 
@@ -28,13 +28,13 @@ function App() {
       <Route path="/signup/volunteer" element={<VolunteerSignup />} />
       <Route path="/signup/organization" element={<OrganizationSignup/>}/>
       <Route path="/signup/attachee" element={<AttacheeSignup/>}/>
+      <Route path="/signup/apprentice" element={<ApprenticeSignup/>}/>
+      <Route path="/signup/intern" element={<InternSignup/>}/>
       <Route path="/signup" element={<SignUp/>}/>
       <Route path="/signin" element={<SignIn/>}/>
       <Route path="/foooter" element={<Foooter/>}/>
       <Route path="/footer" element={<Footer/>}/>
       <Route path="/about" element={<About/>}/>
-      <Route path="/volunteerdashboard" element={<VolunteerDashboard/>}/>
-      <Route path="/organizationdashboard" element={<OrganizationDashboard/>}/>
       
 
 
